866406062498cd4251b613369032b47ea5fb62a9
Felipe <jfss2@cin.ufpe.br>
2020-11-30 20:11:56 -0300

    feature(main): create a volume changer and add the song time

--- js/index.js
+++ js/index.js
@@ -3,19 +3,22 @@ const songs = [
         title: "Madagascar",
         singer: "Edy Lemond",
         file: "assets/songs/madagascar.mp3",
-        pic: "https://i.scdn.co/image/ab67616d0000b27303ce1b8ba51780ec976f198c"
+        pic: "https://i.scdn.co/image/ab67616d0000b27303ce1b8ba51780ec976f198c",
+        time: 219
     },
     {
         title: "Skibidi",
         singer: "Little Big",
         file: "assets/songs/skibidi.mp3",
-        pic: "https://i.scdn.co/image/ab67616d0000b273e19d74d74591bd9698f2302e"
+        pic: "https://i.scdn.co/image/ab67616d0000b273e19d74d74591bd9698f2302e",
+        time: 193
     },
     {
         title: "Praeira",
         singer: "Chico Science",
         file: "assets/songs/praeira.mp3",
-        pic: "https://i.scdn.co/image/ab67616d0000b273f56890f136c79550374bc834"
+        pic: "https://i.scdn.co/image/ab67616d0000b273f56890f136c79550374bc834",
+        time: 217
     }
 ]
 
@@ -28,6 +31,7 @@ const play = document.querySelector("#play")
 const pause = document.querySelector("#pause")
 const plus = document.querySelector("#plus")
 const picSong = document.querySelector("#pic-song")
+const volumeRange = document.querySelector("#volume")
 
 play.addEventListener("click", () => {
     progression.max = audio.duration
@@ -46,6 +50,10 @@ plus.addEventListener("click", () => {
     audio.currentTime += 10
 })
 
+volume.addEventListener("input", () => {
+    audio.volume = volumeRange.value / 100
+})
+
 audio.addEventListener("timeupdate", () => {
     progression.value = audio.currentTime
 })
@@ -59,7 +67,7 @@ function changeSong(num) {
     songName.innerHTML = `${songs[num].title}`
     singerName.innerHTML = `${songs[num].singer}`
     audio.src = `${songs[num].file}`
-    progression.max = 10
-    progression.max = audio.duration
+    progression.max = songs[num].time
+
     audio.play()
 }
